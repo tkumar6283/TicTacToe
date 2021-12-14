dbf1637c0f072fef3541ce7f2f1dc7ebb6a69bd1
tkumar6283 <67906805+tkumar6283@users.noreply.github.com>
2021-12-15 01:45:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+console.log("Welcome To Tic TAc toe")
+let turn = "X"
+let gameover = false;
+
+// Function to change the turn
+const changeTurn = () =>{
+    return turn==="X" ? "0" : "X"
+}
+
+// Function to Check For a Win
+const checkWin = () => {
+    let boxtexts = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText ) 
+        && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) 
+        && (boxtexts[e[0]].innerText !== ""))
+        {
+        document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " Won "
+        gameover = true;
+        document.querySelector('.imgg').getElementsByTagName('img')[0].style.width = "56px"
+        }
+    })
+  }
+
+// Main logic 
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext')
+    element.addEventListener('click', ()=> {
+        if (boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            checkWin();
+            if (!gameover)
+            {
+            document.getElementsByClassName("info")[0].innerText = " turn for " + turn;
+            } 
+        }
+    })
+})
+
+// reset button
+reset.addEventListener('click' , () => {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X";
+    gameover = false
+    document.getElementsByClassName("info")[0].innerText = " turn for " + turn;
+    document.querySelector('.imgg').getElementsByTagName('img')[0].style.width = "0px"
+})
